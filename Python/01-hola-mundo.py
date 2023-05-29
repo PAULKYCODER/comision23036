26ab9b9b38ba44c60f84c9695228efc1118ee46f
Luciano Signorelli <luciano.signorelli@lmvsoftware.com>
2023-05-29 20:33:06 -0300

    Create 01-hola-mundo.py

---
+++ Python/01-hola-mundo.py
@@ -0,0 +1,20 @@
+a = 5
+b = 10
+
+if b > 5:
+    # TODO: Hacer exponencial
+    print(b)
+    pass
+
+
+for i in range(1,10):
+    if i == 5:
+        break
+    print('Valor de i:', i)
+
+def suma(x,z):
+    suma = x + z
+    print(suma)
+
+suma(a,b)
+
